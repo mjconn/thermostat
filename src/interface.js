c8c19cf7d6105df8e028b99297b674380d553920
Mike Connolly <mjc86@hotmail.co.uk>
2021-07-08 15:17:13 +0100

    energy usage colours working!

--- src/interface.js
+++ src/interface.js
@@ -2,22 +2,26 @@ document.addEventListener("DOMContentLoaded", () => {
   thermostat = new Thermostat();
   document.querySelector("#current-temp").innerText = thermostat.temperature + "°C"
   _energyUsage();
+  _energyUsageColor();
 
   document.querySelector("#temp-up").addEventListener ('click', () => {
     thermostat.up();
     _energyUsage();
+    _energyUsageColor();
     document.querySelector("#current-temp").innerText = thermostat.temperature + "°C"
   })
 
   document.querySelector("#temp-down").addEventListener ('click', () => {
     thermostat.down();
     _energyUsage();
+    _energyUsageColor();
     document.querySelector("#current-temp").innerText = thermostat.temperature + "°C"
   })
 
   document.querySelector("#temp-reset").addEventListener ('click', () => {
     thermostat.reset();
     _energyUsage();
+    _energyUsageColor();
     document.querySelector("#current-temp").innerText = thermostat.temperature + "°C"
   })
 
@@ -26,6 +30,7 @@ document.addEventListener("DOMContentLoaded", () => {
     if(thermostat.getCurrentTemp() > 25) {
       thermostat.temperature = 25;
       _energyUsage();
+      _energyUsageColor();
     }
     document.querySelector("#current-temp").innerText = thermostat.temperature + "°C"
   })
@@ -37,13 +42,20 @@ document.addEventListener("DOMContentLoaded", () => {
   function _energyUsage() {
     if(thermostat.energyUsage() === 'low-usage') {
       document.querySelector("#energy-usage").innerHTML = "Low Usage";
-      document.querySelector("#energy-usage").classList.add("badge-success");
     } else if (thermostat.energyUsage() === 'medium-usage') {
       document.querySelector("#energy-usage").innerHTML = "Medium Usage";
-      document.querySelector("#energy-usage").classList.add("badge-dark");
     } else {
       document.querySelector("#energy-usage").innerHTML = "High Usage";
-      document.querySelector("#energy-usage").classList.add("badge-danger");
+    }
+  }
+
+  function _energyUsageColor() {
+    if(thermostat.energyUsage() === 'low-usage') {
+      document.querySelector("#energy-usage").className = "badge bg-success";
+    } else if (thermostat.energyUsage() === 'medium-usage') {
+      document.querySelector("#energy-usage").className = "badge bg-dark";
+    } else {
+      document.querySelector("#energy-usage").className = "badge bg-danger";
     }
   }
 });
